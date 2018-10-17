1a6220d77a749bad1af4c51675f9a7a088a6ab82
Thien Phuc <ttran.phuc99@gmail.com>
2018-10-17 17:06:39 +0700

    doing pop up

--- menu-js/set-popup.js
+++ menu-js/set-popup.js
@@ -1,10 +1,23 @@
-$(window).resize(function() {
-  setPos();
+$(".bnt-add-to-cart").click(function() {
+  var index = $(".bnt-add-to-cart").index(this);
+
+  if (index % 2 == 0) {
+    $(".wrap-detail").eq(index/2).css("display", "flex");
+    setPos(index/2);
+  }
+
+  $(window).resize(function() {
+    setPos(index/2);
+  });
 });
 
-function setPos() {
-  var width = $(".detail").width()/2;
-  var height = -($(".detail").height()/2);
+$(".wrap-close-icon").click(function() {
+  $(".wrap-detail").css("display", "none");
+});
+
+function setPos(index) {
+  var width = $(".detail").eq(index).width()/2;
+  var height = -($(".detail").eq(index).height()/2);
 
   height -= 10;
   width += 25;
