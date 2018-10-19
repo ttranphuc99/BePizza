db84e7d187a999b12979c5d0e9cf0d622a0c6768
Thien Phuc <ttran.phuc99@gmail.com>
2018-10-19 17:14:19 +0700

    doing cart

---
+++ menu-js/set-cart.js
@@ -0,0 +1,19 @@
+var crust = ["Thin Crust", "Hand Tossed", "New York Crust"];
+
+var size = ['7"', '9"', '12"'];
+
+var signature_name = ["MEAT LOVER", "EXTRAVAGANZA", "PRIME BEEF", "SINGAPORE SEAFOOD", "ALMOND CITRUS SEAFOOD"];
+
+var signature_price = ["199,000 đ", "299,000 đ"];
+
+var premium_name = ["TERIYAKI CHICKEN", "PEPPERONI FEAST", "SURF & TURF", "SEAFOOD DELIGHT", "OCEAN MANIA"];
+
+var premium_price = ["79,000 đ", "149,000 đ", "229,000 đ"];
+
+var favorite_name = ["CHESSY CHICKEN BACON", "CHEESE MANIA", "HAWAIIAN", "KID MANIA", "BEFFIE MANIA"];
+
+var favorite_price = ["59,000 đ", "129,000 đ", "189,000 đ"];
+
+var element = '' 
+<div class="wrap-order">
+'
